878b4b0b84bd3aaed7166c720aee4d82a359bfd0
TOLGA <exbert@exbert.com>
2022-07-02 01:29:07 +0200

    page structure 1

--- app.js
+++ app.js
@@ -2,5 +2,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// Template Engine configuration
+app.set("view engine", "ejs");
+
+// Middleware configuration
+
+app.use(express.static("public"));
+
 app.listen(port, () => console.log(`App started on port ${port}!`));
+
+// Route configuration
+
+app.get("/", (req, res) => {
+    res.status(200).render("index", { page_name: "index" });
+});
+
+app.get("/about", (req, res) => {
+    res.status(200).render("about", { page_name: "about" });
+});
